b45fd15d14cc0562c3ee8f63a07af588234afbf7
lexkde <yanjinyi@mail.sim.ac.cn>
2016-10-28 14:16:55 +0800

    数据通道打开，调试完成。可以写功能界面了。打包给hardware咯

--- app/src/main/java/geekband/yanjinyi1987/com/bluetoothcomm/MainActivity.java
+++ app/src/main/java/geekband/yanjinyi1987/com/bluetoothcomm/MainActivity.java
@@ -1,11 +1,14 @@
 package geekband.yanjinyi1987.com.bluetoothcomm;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,17 +17,69 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 import geekband.yanjinyi1987.com.bluetoothcomm.fragment.BluetoothConnection;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public static final int DEVICE_CONNECTED = 1;
+    public static final int MESSAGE_READ=0;
     private BluetoothAdapter mBluetoothAdapter;
     private boolean noBluetooth;
     private boolean bluetoothDisable;
     private static final int REQUEST_ENABLE_BT=1;
-    private static ArrayList<String> mConnectedBTDevices = new ArrayList<>();
+    private ArrayList<String> mConnectedBTDevices = new ArrayList<>();
+    private boolean rwReady=false;
+
+    //与fragmentDialog通信的Handler
+    private Handler mMainActivityHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            BluetoothDevice mBluetoothDevice = null;
+            BluetoothSocket mBluetoothSocket=null;
+            switch(msg.what) {
+                case DEVICE_CONNECTED:
+                    mBluetoothDevice = (BluetoothDevice) (((ArrayList<Object>) msg.obj).get(0));
+                    mBluetoothSocket = (BluetoothSocket) (((ArrayList<Object>) msg.obj).get(1));
+
+                    //建立读写通道哦！UI主线程与读写线程的交互
+                    mSSPRWThread = new SSPRWThread(mBluetoothSocket);
+                    mSSPRWThread.start();
+                    //将功能区使能
+                    rwReady=true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    //与读写线程通信的Handler
+    private Handler mReadSSPHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case MESSAGE_READ:
+                    int length = msg.arg1;
+                    byte[] info =(byte[])(msg.obj); //问题在于这里直接引用了buffer，而buffer的数据会被read覆盖，这样造成了问题。
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for(int i=0;i<length;i++) {
+                        Log.i("byte in handler",String.valueOf((char)info[i]));
+                        stringBuilder.append((char)info[i]);
+                    }
+                    String strInfo = stringBuilder.toString();
+                    Log.i("MainActivity",strInfo);
+                    mReceivedSPPDataText.setText(mReceivedSPPDataText.getText()+strInfo);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     //处理蓝牙接收器的状态变化
     /**
@@ -36,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      *int STATE_TURNING_ON: Indicates the local Bluetooth adapter is turning on.
      *                      However local clients should wait for STATE_ON before attempting to use the adapter.
      */
-    BroadcastReceiver mBroadcstReceiver = new BroadcastReceiver() {
+    BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -46,19 +101,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 switch (state) {
                     case BluetoothAdapter.STATE_TURNING_OFF:
                         mConnectedBTDevices.clear(); //清除连接列表
-                        //关闭读写通道？
+                        //关闭读写通道
+                        if(rwReady==true) {
+                            mSSPRWThread.cancel();
+                        }
+                        //disable功能区
+                        rwReady=false;
                         break;
                     case BluetoothAdapter.STATE_ON:
-                        if(noBluetooth==false) {
-                            callBtConnectionDialog();
-                        }
-                        else {
-                            try {
-                                throw(new Exception("程序不可能运行到这里"));
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
+
                         break;
                     default:
                         break;
@@ -70,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mAtCommandText;
     private Button mSendAtCommandButton;
     private EditText mReceivedSPPDataText;
+    private SSPRWThread mSSPRWThread;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -85,6 +137,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     //但是打开的这个Activity好像只有显示配对和查找配对设备的功能，没有连接的功能哦。
                     startActivity(settingsIntent);
                     */
+                    if(noBluetooth==false) {
+                        callBtConnectionDialog();
+                    }
+                    else {
+                        try {
+                            throw(new Exception("程序不可能运行到这里"));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
                 else if(resultCode == RESULT_CANCELED) {
                     //蓝牙设备没有被使能
@@ -100,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void callBtConnectionDialog() {
-        BluetoothConnection btDialog = BluetoothConnection.newInstance(mBluetoothAdapter,mConnectedBTDevices);
+        BluetoothConnection btDialog = BluetoothConnection.newInstance(mBluetoothAdapter,mConnectedBTDevices,mMainActivityHandler);
         btDialog.show(getFragmentManager(), "蓝牙设置");
     }
 
@@ -113,13 +175,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         //注册接受器
-        registerReceiver(mBroadcstReceiver,intentFilter);
+        registerReceiver(mBroadcastReceiver,intentFilter);
     }
 
     @Override
     protected void onDestroy() {
+        Log.i("MainActivity","onDestroy");
         super.onDestroy();
-        unregisterReceiver(mBroadcstReceiver);
+        unregisterReceiver(mBroadcastReceiver);
+        if(rwReady==true) {
+            mConnectedBTDevices.clear();
+            mSSPRWThread.cancel();
+        }
     }
 
     /*
@@ -168,11 +235,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String at_command = mAtCommandText.getText().toString();
                 if(at_command!=null && at_command.length()>0) {
                     //发送命令
+                    mSSPRWThread.write(at_command.getBytes());
                 }
                 break;
             default:
                 break;
         }
 
+    }
+
+    private class SSPRWThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public SSPRWThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            StringBuilder stringBuilder = new StringBuilder();
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer); //这种问题应该怎么处理呢？
+                    // Send the obtained bytes to the UI activity
+                    mReadSSPHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
     }
 }
